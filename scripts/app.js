020aae9486e96edafe23911df217b2e78b0520bd
EVYN ROSE <evynrosebud@gmail.com>
2024-02-20 10:11:18 +0000

    this is my second commit. this is the basics of the css and html done for.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+// 1) grid with correct numbers
+// 2) create cells  (can i click and change the background?)
+// 3) can the players take turns?
+// 4) how can you tell someone has won the game? - how they've won,
+
+//focus on fundamentals,
