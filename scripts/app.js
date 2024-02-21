113f890f2ef4976df1aa74a97f9df67b6787966c
EVYN ROSE <evynrosebud@gmail.com>
2024-02-21 09:23:42 +0000

    this is my third commit. i have a working basic game.

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,171 @@
 // 4) how can you tell someone has won the game? - how they've won,
 
 //focus on fundamentals,
+
+// // Function to create the game board
+// function createBoard() {
+//   var board = document.getElementById("board");
+//   for (var i = 0; i < 6; i++) {
+//     for (var j = 0; j < 6; j++) {
+//       var cell = document.createElement("div");
+//       cell.classList.add("cell");
+//       cell.addEventListener("click", function () {
+//         this.style.backgroundImage = "url('images/kitten.png')"; // Change the background image of cell
+//         this.style.backgroundSize = "cover";
+//       });
+//       board.appendChild(cell);
+//     }
+//   }
+// }
+
+// // Call the createBoard function to generate the game board
+// createBoard();
+
+var playerKitten = "K";
+var playerPuppy = "P";
+var currPlayer = playerKitten;
+
+var gameOver = false;
+var board;
+
+var rows = 6;
+var columns = 7;
+var currColumns = []; //keeps track of which row each column is at.
+
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  board = [];
+  currColumns = [5, 5, 5, 5, 5, 5, 5];
+
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+      // JS
+      row.push(" ");
+      // HTML
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.classList.add("tile");
+      tile.addEventListener("click", setPiece);
+      document.getElementById("board").append(tile);
+    }
+    board.push(row);
+  }
+}
+
+function setPiece() {
+  if (gameOver) {
+    return;
+  }
+
+  //get coords of that tile clicked
+  let coords = this.id.split("-");
+  let r = parseInt(coords[0]);
+  let c = parseInt(coords[1]);
+
+  // figure out which row the current column should be on
+  r = currColumns[c];
+
+  if (r < 0) {
+    // board[r][c] != ' '
+    return;
+  }
+
+  board[r][c] = currPlayer; //update JS board
+  let tile = document.getElementById(r.toString() + "-" + c.toString());
+  if (currPlayer == playerKitten) {
+    tile.classList.add("kitten-piece");
+    currPlayer = playerPuppy;
+  } else {
+    tile.classList.add("puppy-piece");
+    currPlayer = playerKitten;
+  }
+
+  r -= 1; //update the row height for that column
+  currColumns[c] = r; //update the array
+
+  checkWinner();
+}
+
+function checkWinner() {
+  // horizontal
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r][c + 1] &&
+          board[r][c + 1] == board[r][c + 2] &&
+          board[r][c + 2] == board[r][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  // vertical
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 3; r++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r + 1][c] &&
+          board[r + 1][c] == board[r + 2][c] &&
+          board[r + 2][c] == board[r + 3][c]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  // anti diagonal
+  for (let r = 0; r < rows - 3; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r + 1][c + 1] &&
+          board[r + 1][c + 1] == board[r + 2][c + 2] &&
+          board[r + 2][c + 2] == board[r + 3][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  // diagonal
+  for (let r = 3; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r - 1][c + 1] &&
+          board[r - 1][c + 1] == board[r - 2][c + 2] &&
+          board[r - 2][c + 2] == board[r - 3][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+}
+
+function setWinner(r, c) {
+  let winner = document.getElementById("winner");
+  console.log(winner);
+  if (board[r][c] == playerKitten) {
+    winner.innerText = "Kitten Wins";
+  } else {
+    winner.innerText = "Puppy Wins";
+  }
+  gameOver = true;
+}
+
+let button = document.getElementById("resetbutton");
+button.addEventListener("click", () => location.reload());
